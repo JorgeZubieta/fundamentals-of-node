5cda3c796822976c5152738d051f26f4956f9ec5
jorge zubieta <zubietajor@gmail.com>
2021-08-06 11:03:52 -0300

    assyncAwait added

--- async/asyncAwait.js
+++ async/asyncAwait.js
@@ -1,5 +1,5 @@
-// Ejemplo promesa
-function hola (nombre) {
+// Agregamos en las funciones solo async al principio!
+async function hola (nombre) {
     return new Promise(function(resolve, reject){
         setTimeout(function() {
             console.log("Hola " + nombre);
@@ -11,17 +11,18 @@ function hola (nombre) {
 }
 
 // funion que se repetira
-function hablar(nombre) {
+async function hablar(nombre) {
     return new Promise( (resolve, reject) => {
         setTimeout(function() {
             console.log("Bla bla bla...");
             // resolve(nombre);
-            reject('Reject activado!');
+            resolve('blo blo blo...');
+            // reject('Reject activado!');
         },1000);
     });
 }
 
-function adios (nombre) {
+async function adios (nombre) {
     return new Promise ( (resolve, reject) => {
         setTimeout(function() {
             console.log("Adios " + nombre);
@@ -30,3 +31,23 @@ function adios (nombre) {
         }, 1000);
     })
 }   
+
+// llamado por Async y ejecutamos await! pero el await debe ir si o si dentro de una funcion
+async function main () {
+    let nombre = await hola('Kaito-Kun');
+    await hablar();
+    // sin el await no espera que se cumpla la funcion hablar y ejecuta proceso terminado!
+    hablar();
+    await hablar();
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Proceso terminado con await!');
+}
+
+// INICIAMOS EL LLAMADO : se cumple en orden con todos los await en hablar!
+console.log('Iniciando proceso:');
+main();
+// con este console log aca y sin el waiat en hola, esto funcionara en forma asincrona y es lo que tenemos que evitar para que el proceso se vea o parezca asincrono!
+console.log('Proceso terminado sin await!');
